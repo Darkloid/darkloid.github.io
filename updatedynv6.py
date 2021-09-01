a79d55a4957d916d4fa8d78d8f3a0de28355b568
Darkloid <darkloid@outlook.com>
2021-09-01 15:31:12 +0800

    Update updatedynv6.py

--- updatedynv6.py
+++ updatedynv6.py
@@ -6,7 +6,7 @@ getIPV6_process = subprocess.Popen("ipconfig", stdout = subprocess.PIPE)
 
 output = (getIPV6_process.stdout.read())
 
-ipv6_pattern='2403:ac00:[0-9a-f:]+'
+ipv6_pattern='***:***:[0-9a-f:]+'
 ipv4_pattern='172\.20\.[0-9]{1,3}\.[0-9]{1,3}'
 ipv4_local_pattern='172\.17\.[0-9]{1,3}\.[0-9]{1,3}'
 
@@ -25,20 +25,20 @@ else:
     ipv4 = ''
 
 print(ipv4)
-url = "http://dynv6.com/api/update?hostname=darkloid.dynv6.net&token=C745iSAq5fQyfubay_V6YWP2_b6_rL&ipv4=" + ipv4
+url = "http://dynv6.com/api/update?hostname=***&token=***&ipv4=" + ipv4
 response = requests.get(url=url)
 print(response.text)
 
 print(ipv6)
-# url = "http://dynv6.com/api/update?hostname=darkloid.dynv6.net&token=C745iSAq5fQyfubay_V6YWP2_b6_rL&ipv6=" + ipv6
+# url = "http://dynv6.com/api/update?hostname=***&token=***&ipv6=" + ipv6
 # response = requests.get(url=url)
 # print(response.text)
 
 print(ipv4_local)
-url = "http://dynv6.com/api/update?hostname=darkloid-local.dynv6.net&token=erySEmcsdzBHtxTU8omU1XCyScgyJU&ipv4=" + ipv4_local
+url = "http://dynv6.com/api/update?hostname=***&token=***&ipv4=" + ipv4_local
 response = requests.get(url=url)
 print(response.text)
 
-url = "http://iyuu.cn/IYUU6284T8564b4f14627a55d57f5bcdca4bc235701b4654a.send?text=NAS%20IP地址已更新&desp=" + ipv4 + "\n" + ipv4_local + "\n" + ipv6
+url = "http://iyuu.cn/IYUU***.send?text=NAS%20IP地址已更新&desp=" + ipv4 + "\n" + ipv4_local + "\n" + ipv6
 response = requests.get(url=url)
 print(response.text)
